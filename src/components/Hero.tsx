b90eb0ebc699be0e5de696ceb95c2ae32c5e7ea9
Debajit91 <debajitroy544@gmail.com>
2025-11-25 21:01:56 +0600

    initilized hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export function Hero() {
+  return (
+    <section className="bg-primary text-white py-16">
+      <div className="container">
+        <h1 className="text-4xl md:text-5xl font-bold mb-4">BREAKFAST</h1>
+        <p className="max-w-xl text-sm md:text-base opacity-90">
+          Breakfast, often referred to as the ‘most important meal of the day’, provides essential nutrients to kick start our day. It includes a variety of foods, like fruits, cereals, dairy products, and proteins, that contribute to a balanced diet. 
+        </p>
+      </div>
+    </section>
+  );
+}
