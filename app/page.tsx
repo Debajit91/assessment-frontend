02e6cc2313c0576164435d04338b2ba2c48bfc14
Debajit91 <debajitroy544@gmail.com>
2025-11-25 22:25:27 +0600

    Navbar fully responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
-import Image from "next/image";
+import { Hero } from "@/src/components/Hero";
+import { Navbar } from "@/src/components/Navbar";
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
+    
       <main className="min-h-screen bg-white">
-        <div className="max-w-6xl mx-auto px-4">
-          <h1 className="text-3xl font-bold mt-10">Restaurant Assessment UI</h1>
-          <p className="mt-2 text-gray-600">
-            Frontend setup successful. Start building sections here.
-          </p>
-        </div>
+        <Navbar/>
+        <Hero/>
       </main>
-    </div>
+    
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 export function Navbar() {
   return (
-    <header className="w-full bg-primary text-white">
-      <div className="container flex items-center justify-between py-4">
-        <div className="font-semibold tracking-wide text-lg">RESTAURANT</div>
-        <div className="w-full max-w-sm">
+    <header className="w-full bg-[#880808] text-[#FFFFFF]">
+      <div className="container flex flex-col md:flex-row md:items-center md:justify-between p-8 gap-4">
+        <div className="hidden md:block font-semibold tracking-wide text-3xl md:none">RESTAURANT</div>
+
+        <div className="relative w-full md:max-w-[750px] h-8 md:h-11">
           <input
-            className="w-full rounded-full px-4 py-2 text-sm text-dark outline-none"
+            className="w-full h-full rounded-xl pl-14 md:pl-18 pr-4 text-sm md:text-base text-black font-bold bg-[#FFFFFF] outline-none"
             placeholder="Search..."
           />
+
+          <span className="absolute left-5 top-1/2 -translate-y-1/2 text-gray-500">
+          <MagnifyingGlassIcon className="w-6 h-6" />
+        </span>
         </div>
+        
       </div>
     </header>
   );
